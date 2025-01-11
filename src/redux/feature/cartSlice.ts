7cd7a14d73defaccedc40674ef569d8383b4b01d
Huzaifa Mukhtar <mukhtarhuzaifa436@gmail.com>
2025-01-12 00:41:55 +0500

    update

---
+++ src/redux/feature/cartSlice.ts
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+    name: "cart",
+    initialState: {
+        cart: []
+    },
+    reducers: {
+
+    }
+})
+
+export default cartSlice.reducer 
