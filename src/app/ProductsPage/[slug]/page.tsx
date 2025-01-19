f66f1131a57e4608d0f59524d6873d7c90083d42
Huzaifa Mukhtar <mukhtarhuzaifa436@gmail.com>
2025-01-19 23:38:36 +0500

    Update page.tsx

--- src/app/ProductsPage/[slug]/page.tsx
+++ src/app/ProductsPage/[slug]/page.tsx
@@ -263,43 +263,43 @@ const Page = ({ params }: { params: { slug: string } }) => {
     </div>
   );
 };
-
-export default Page;
 interface RatingProps {
   rating: number; // e.g., 4.5
 }
 
 export function Rating({ rating }: RatingProps) {
-  const fullStar = Math.floor(rating);
-  const halfStar = rating % 1 !== 0;
+  const fullStars = Math.floor(rating);
+  const hasHalfStar = rating % 1 !== 0;
+
   return (
     <div className="flex gap-3">
       <div className="star active flex items-center gap-1">
-        {Array.from({ length: fullStar }, (_, index) => (
-          <div key={index} className="">
+        {Array.from({ length: fullStars }, (_, index) => (
+          <div key={index}>
             <Image
               src={"/products/rating.svg"}
-              alt="Star"
+              alt="Full Star"
               width={25}
               height={25}
             />
           </div>
         ))}
-        {halfStar && <div className="star half active" />}
-        {Array.from({ length: halfStar ? 1 : 0 }, (_, index) => (
-          <div key={index}>
+        {hasHalfStar && (
+          <div>
             <Image
               src={"/products/ratingHalf.svg"}
-              alt="Star"
-              width={12}
-              height={12}
+              alt="Half Star"
+              width={25}
+              height={25}
             />
           </div>
-        ))}
+        )}
       </div>
       <div>
-        <span className="text-black text-xl">{rating}</span>/{5}
+        <span className="text-black text-xl">{rating}</span>/5
       </div>
     </div>
   );
 }
+
+export default Page;
