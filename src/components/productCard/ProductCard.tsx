46bbfae9f6317c83631c924e889516a25fa74eef
Huzaifa Mukhtar <mukhtarhuzaifa436@gmail.com>
2025-01-25 01:43:09 +0500

    Update ProductCard.tsx

--- src/components/productCard/ProductCard.tsx
+++ src/components/productCard/ProductCard.tsx
@@ -1,19 +1,38 @@
-import React from "react";
+"use client";
+import React, { useEffect, useRef } from "react";
 import Image from "next/image";
 import { Product } from "../../../Typing";
 // import { Rating } from "../../app/ProductsPage/[slug]/page";
 import Rating from "../Rating/Rating";
 import Link from "next/link";
+import VanillaTilt from "vanilla-tilt";
 
 function ProductCard({ item }: { item: Product }) {
   const { image, name, rating, _id, price, discountPercent, discountedPrice } =
     item;
+
+  const tiltRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (tiltRef.current) {
+      VanillaTilt.init(tiltRef.current, {
+        max: 35, // Maximum tilt angle
+        perspective: 2000, // Defines the 3D depth (the higher, the more dramatic the effect)
+        speed: 400, // Speed of the tilt effect
+        easing: "cubic-bezier(.03,.98,.52,.99)", // Smooth easing for a better experience
+      });
+    }
+  }, []);
   // const id = `${title.replace(/-/g, "_")}${new Date().getMilliseconds()}`;
   return (
     <Link href={`/ProductsPage/${_id}`}>
       <div
         id={_id}
+        ref={tiltRef}
         className={`hover:shadow-xl p-3 grid w-[290px] justify-between  overflow-hidden] `}
+        style={{
+          transformStyle: "preserve-3d",
+        }}
       >
         <Image
           src={image}
@@ -21,6 +40,9 @@ function ProductCard({ item }: { item: Product }) {
           width={290}
           height={298}
           className="mb-[5px] w-[290px] h-[298px] object-cover rounded-[20px] object-center"
+          style={{
+            transform: "translateZ(40px)",
+          }}
         />
         <h3 className="font-bold text-[20px] capitalize">{name}</h3>
         <div className="rating flex gap-[8px]">
