e9162705bd4ffab2b0e9c0317c5b54309acebe68
Huzaifa Mukhtar <mukhtarhuzaifa436@gmail.com>
2025-02-21 01:56:30 +0500

    Update client.ts

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,6 +1,7 @@
 import { createClient } from 'next-sanity'
 
 import { apiVersion, dataset, projectId, token } from '../env'
+// import { formData } from '../../../Typing';
 
 export const client = createClient({
   projectId,
@@ -14,3 +15,17 @@ export const client = createClient({
 // client
 //   .listen('*[_type == "comment"]')
 // .subscribe(update => console.log("Live update:", update))
+
+// async function deleteAllComments() {
+//   const comments = await client.fetch(`*[_type == "comment"]{_id}`);
+
+//   const deletePromises = comments.map((comment: formData) =>
+//     client.delete(comment._id)
+//   );
+
+//   await Promise.all(deletePromises);
+
+//   console.log("✅ All comments deleted successfully!");
+// }
+
+// deleteAllComments();
